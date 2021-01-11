915e040ae17c9c769a3bef2445922d561a547955
TotosL <totosluke@gmail.com>
2021-01-11 11:56:59 +0200

    Implemented drawing servos.

---
+++ Drawing Test/Drawing Test/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Drawing_Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(334, 124);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(422, 22);
+            this.tbInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(422, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter the position or degrees desired";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(540, 613);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Current position";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 648);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(603, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(766, 124);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 22);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Actuate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1271, 778);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Drawing Test/Drawing Test/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Drawing_Test
+{
+    public partial class Form1 : Form
+    {
+        public double CenterX { get; set; }
+        public double CenterY { get; set; }
+        public delegate void writeText(string x);
+        string degrees = "0";
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1 = new SerialPort("COM8", 115200);
+            serialPort1.DtrEnable = true;
+            serialPort1.Open();
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string line = serialPort1.ReadLine();
+            this.BeginInvoke(new writeText(WriteData), new object[] { line });
+            paintServo(400, 300, 100, 100);
+        }
+        private void WriteData(string line)
+        {
+            string[] splitdata = line.Split(',');
+            string[] s1 = splitdata[14].Split(':');
+            degrees = s1[1];
+            textBox1.Text = degrees;     
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string state = tbInput.Text;
+            if (state.Contains("Isolate"))
+            {
+                serialPort1.WriteLine("FV1:I");
+            }
+            else if (state.Contains("Feed"))
+            {
+                serialPort1.WriteLine("FV1:F");
+            }
+            else if (state.Contains("Vent"))
+            {
+                serialPort1.WriteLine("FV1:V");
+            }
+        }
+        
+        private void paintServo(int x, int y, int width, int height)
+        {
+            float centerX = x + width / 2;
+            float centerY = y + height / 2;
+            float deg = float.Parse(degrees);
+
+            Graphics g = this.CreateGraphics();
+            g.Clear(Color.White);
+            Pen pen = new Pen(Color.Black, 2f);
+            SolidBrush brush = new SolidBrush(Color.Black);
+            g.DrawRectangle(pen, x, y, width, height);
+            g.TranslateTransform(centerX, centerY);
+            g.RotateTransform(deg);
+            g.TranslateTransform(-centerX, -centerY);
+            g.DrawRectangle(pen, x + (4 * width) / 10, y, width / 5, height);
+            g.FillRectangle(brush, x + (4 * width) / 10, y, width / 5, height);
+            g.DrawRectangle(pen, x, y + (4 * height) / 10, (6 * width) / 10, height / 5);
+            g.FillRectangle(brush, x, y + (4 * height) / 10, (6 * width) / 10, height / 5);
+        }
+    }
+}
